42fe8cb222422679fbe9affc57a13fc7bc32bd9b
Ray <loveyou520babytw@gmail.com>
2019-02-11 09:32:24 +0800

    加入eslint

--- js/all.js
+++ js/all.js
@@ -1,50 +1,49 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        data: [
-            {
-                name: '巧呼呼蘇打水',
-                price: 30,
-                expiryDate: 90
-            },
-            {
-                name: '心驚膽跳羊肉飯',
-                price: 65,
-                expiryDate: 2
-            },
-            {
-                name: '郭師傅武功麵包',
-                price: 32,
-                expiryDate: 1
-            },
-            {
-                name: '不太會過期的新鮮牛奶',
-                price: 75,
-                expiryDate: 600
-            },
-            {
-                name: '金殺 巧粒粒',
-                price: 120,
-                expiryDate: 200
-            }
-        ],
-        sortName: '',
-        isRotateValue: false,
-    },
-    // 請在此撰寫 JavaScript
-    computed: {
-        sortData: function () {
-            var vm = this;
-            if (vm.isRotateValue) {
-                return vm.data.sort(function sortNumber(a, b) {
-                    return b[vm.sortName] - a[vm.sortName];
-                });
-            } else if (!vm.isRotateValue) {
-                return vm.data.sort(function sortNumber(a, b) {
-                    return a[vm.sortName] - b[vm.sortName];
-                });
-            }
-
-        }
+  el: '#app',
+  data: {
+    data: [
+      {
+        name: '巧呼呼蘇打水',
+        price: 30,
+        expiryDate: 90
+      },
+      {
+        name: '心驚膽跳羊肉飯',
+        price: 65,
+        expiryDate: 2
+      },
+      {
+        name: '郭師傅武功麵包',
+        price: 32,
+        expiryDate: 1
+      },
+      {
+        name: '不太會過期的新鮮牛奶',
+        price: 75,
+        expiryDate: 600
+      },
+      {
+        name: '金殺 巧粒粒',
+        price: 120,
+        expiryDate: 200
+      }
+    ],
+    sortName: '',
+    isRotateValue: false
+  },
+  // 請在此撰寫 JavaScript
+  computed: {
+    sortData: function () {
+      var vm = this
+      if (vm.isRotateValue) {
+        return vm.data.sort(function sortNumber (a, b) {
+          return b[vm.sortName] - a[vm.sortName]
+        })
+      } else if (!vm.isRotateValue) {
+        return vm.data.sort(function sortNumber (a, b) {
+          return a[vm.sortName] - b[vm.sortName]
+        })
+      }
     }
-});
+  }
+})
